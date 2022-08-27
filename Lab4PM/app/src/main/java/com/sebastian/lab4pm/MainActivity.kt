09325c9a85c1c4ca53173300f55d9e85f0ce5fc9
Teviets <est21405@uvg.edu.gt>
2022-08-27 15:08:23 -0600

    Arreglo del ultimo comit

--- Lab4PM/app/src/main/java/com/sebastian/lab4pm/MainActivity.kt
+++ Lab4PM/app/src/main/java/com/sebastian/lab4pm/MainActivity.kt
@@ -2,29 +2,44 @@ package com.Sebastian.lab4plataformasmoviles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.view.Gravity
-import android.view.LayoutInflater
 import android.widget.Toast
 import android.widget.Button
-import android.content.Intent
 import com.sebastian.lab4pm.R
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var button: Button
+    lateinit var ToastMessageName: Button
+    lateinit var descarga: Button
+    lateinit var detalles: Button
+    lateinit var locacion: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button = findViewById(R.id.button4);
+        ToastMessageName = findViewById(R.id.ToastMessage);
+        descarga = findViewById(R.id.boton_Descarga);
+        detalles = findViewById(R.id.Details);
+        locacion = findViewById(R.id.LocationRes);
 
         initListener();
     }
     fun initListener(){
-        button.setOnClickListener{
+        ToastMessageName.setOnClickListener{
             Toast.makeText(this, "Sebastian Estrada Tuch", Toast.LENGTH_LONG).show()
         }
+        descarga.setOnClickListener{
+           /* val webIntent: Intent = Uri.parse("https://www.android.com").let { webpage ->
+                Intent(Intent.ACTION_VIEW, webpage)
+            }*/
+        }
+        detalles.setOnClickListener{
+
+        }
+        locacion.setOnClickListener {
+
+        }
+
     }
 }
